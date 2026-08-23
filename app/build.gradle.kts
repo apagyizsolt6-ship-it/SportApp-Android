97d2607f0449574712239bd6ed7515ddb3494b27
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-23 19:36:33 +0200

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    // StatPal csapat- és ligaképek megjelenítése
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     // Retrofit 2
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
